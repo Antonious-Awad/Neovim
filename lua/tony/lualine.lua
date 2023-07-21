3a0ba47795a78af68bd432c98930969042cbee5c
Antonious-Awad <antonuostony1@gmail.com>
2023-07-21 12:12:31 +0300

    added updated lualine

--- lua/tony/lualine.lua
+++ lua/tony/lualine.lua
@@ -5,12 +5,12 @@ lualine.setup {
   options = {
     icons_enabled = true,
     theme = 'nord',
-    section_separators = { left = '', right = '' },
-    component_separators = { left = '', right = '' },
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
     disabled_filetypes = {}
   },
   sections = {
-    lualine_a = { 'mode' },
+    lualine_a = {  { 'mode', separator = { left = '' }, right_padding = 2 }, },
     lualine_b = { 'branch' },
     lualine_c = { {
       'filename',
@@ -24,7 +24,7 @@ lualine.setup {
       'filetype'
     },
     lualine_y = { 'progress' },
-    lualine_z = { 'location' }
+    lualine_z = { { 'location', separator = { right = '' }, left_padding = 2 } }
   },
   inactive_sections = {
     lualine_a = {},
